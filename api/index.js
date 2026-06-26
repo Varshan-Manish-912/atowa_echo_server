e3bf0c84326d0039fcfa41e83210253b84439333
Varshan Manish <varshan.manish.work@gmail.com>
2026-06-26 22:23:36 +0530

    CORS policy changes

--- api/index.js
+++ api/index.js
@@ -5,6 +5,14 @@ let backend = {
 
 export default function handler(req, res) {
 
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+    if (req.method === "OPTIONS") {
+        return res.status(200).end();
+    }
+
     if (req.method === "GET") {
 
         return res.status(200).json(backend);
